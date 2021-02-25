f7953463aeab908cd1819447f9ce0b29f6a1063f
Vivi <jiahui1001@yeah.net>
2021-02-25 22:56:05 +0800

    准备上传了

--- src/index.js
+++ src/index.js
@@ -1,2 +1,18 @@
 import x from './x.js'
-console.log('x')
+import png from './assets/1.png'
+
+
+const div = document.getElementById('app')
+div.innerHTML = `
+  <img src ="${png}">
+`
+const button = document.createElement('button')
+div.appendChild(button)
+button.innerText = '懒加载'
+button.click = () => {
+  const promise = import('./lazy')
+  promise.then((module) => {
+    const fn = module.default
+    fn()
+  }), () => { }
+}
